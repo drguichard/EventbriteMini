99eec0b6a84b68b7e500cea0c5d307f213c31ab0
TG <tguichard8@hotmail.com>
2019-11-04 16:27:29 +0100

    make it better again

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,18 +1,38 @@
 class Event < ApplicationRecord
-    validates :start_date, presence: true
-    validates :duration, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
-    validates :divisible_by_five
-
-    validates :title, presence: true, length: { in: 5..140 }
-    validates :description, presence: true, length: { in: 20..1000}
-    validates :price, presence: true
-    validates_inclusion_of :price, :in => 1..1000
-    validates :location, presence: true
+  has_many :participations
+  has_many :users, through: :participations
+  belongs_to :user
   
-    def divisible_by_five
-      if (self.duration % 5) != 0
-        self.errors[:base] << "Number must be divisible by 5!"
-      end
-    end
 
+  validate :divisible_by_five, :is_in_future?
+
+  validates :start_date, 
+    presence: {message: "Merci de saisir une date !"} #,
+    #if is_in_the_futur?
+    #il est impossible de créer ou modifier un événement dans le passé.
+  validates :duration, 
+    presence: {message: "Merci de saisir une durée !"}
+  validates :title, 
+    presence: {message: "Merci de saisir un titre !"},
+    length: {minimum: 5, maximum: 140, message: "Le titre doit être entre 5 et 140 caractères"}
+  validates :description, 
+    presence: {message: "Merci de saisir un contenu !"},
+    length: {minimum: 20, maximum: 1000, message: "La description doit être entre 20 et 1000 caractères"}
+  validates :price,
+    presence: {message: "Merci de saisir un prix !"},
+    numericality: { in: 1..1000 }
+  validates :location, 
+    presence: {message: "Merci de saisir une ville !"}
+
+
+  def divisible_by_five
+    if (self.duration % 5) != 0
+      self.errors[:base] << "La durée doit être un multiple de 5"
+    end
+  end
+
+  def is_in_future?
+    self.start_date  >=  Date.today
+  end
+  
 end
