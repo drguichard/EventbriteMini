ce02b5a00eedef6f523a21988739da5af6adbeac
TG <tguichard8@hotmail.com>
2019-11-06 10:44:32 +0100

    add events on homepage, show event page, resources prepared on controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'events#index' #root indique la route vers homepage par le controller Events et sa methode index puis affiche la view events/index.html.erb
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  resources :events
+
 end
